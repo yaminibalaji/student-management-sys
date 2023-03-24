59ed990c79b259a00662e12e3341f2a6fe6fef4d
yaminibalaji <yaminibalaji03@gmail.com>
2023-03-24 12:13:43 +0530

    sms in spring boot , thymleaf, jpa, html, css

---
+++ src/main/java/sample/sms/pract/service/StudentService.java
@@ -0,0 +1,22 @@
+package sample.sms.pract.service;
+
+import java.util.List;
+
+import sample.sms.pract.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	void deleteStudentById(Long id);
+
+	
+
+
+
+}
